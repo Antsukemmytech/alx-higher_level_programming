578e0d559c0725f3354fea8f96ff7b2690ad3e35
antsukemmytech <antsukemmytech@gmail.com>
2023-12-04 13:48:59 -0800

    string mainipulation now

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(sting):
     my_string = ""
-    for string in string:
+    for char in string:
         if char != "c" and char != "C":
             my_string += char
     return my_string
