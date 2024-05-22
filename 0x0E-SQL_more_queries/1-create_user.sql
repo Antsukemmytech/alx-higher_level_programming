e360a4b530eb981efdb6fdba3ecd0b934f02fe60
antsukemmytech <antsukemmytech@gmail.com>
2024-05-22 11:13:23 -0700

    user with all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- This  lists all privileges of the MySQL users user_0d_1 and user_0d_2 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
+-- This script create a user with all global privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
