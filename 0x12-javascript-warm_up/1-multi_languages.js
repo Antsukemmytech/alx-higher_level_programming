a3e74d892d1e3e74b9f6fc8a82a9082984b2327b
Antsukemmy <antsukemmytech@gmail.com>
2024-06-20 20:42:30 -0700

    all the languages are good

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+//script that prints 3 lines c is fun, Python is cool and JavaScript is amazing
+
+const myVar = 'C is fun\nPython is cool\nJavascript is amazing;
+console.log(myVar);
+
