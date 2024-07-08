f3ebcc84d61f90e58bcd5b9d5d93c0c4683036bc
Antsukemmytech <antsukemmytech@gmail.com>
2024-07-08 10:04:46 -0700

    tak 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
+"""Fetches https://github.com/Antsukemmytech/alx-higher_level_programming."""
 import urllib.request
 
-
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    request = urllib.request.Request("https://github.com/Antsukemmytech/alx-higher_level_programming")
     with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        x_request_id = response.headers.get('X-Request-Id')
+        print("X-Request-Id:", x_request_id)
 
